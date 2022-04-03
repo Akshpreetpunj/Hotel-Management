b6f960efc9c81e8b913f4fc2b3a52aa30548950b
Akshpreet Punj <000820040@mohawkcollege.ca>
2022-04-02 23:20:28 -0400

    Added Seeders

--- resources/views/rooms/index.blade.php
+++ resources/views/rooms/index.blade.php
@@ -3,4 +3,33 @@
 @section('content')
     <h1>Hotel Rooms</h1>
     <a href="/rooms/create" type="button" class="btn btn-success btn-sm mb-3">Create New Rooms</a>
+
+    <table class="table">
+        <thead class="thead-dark">
+            <tr>
+                <th>Room Number</th>
+                <th>Room Name</th>
+                <th>Room Description</th>
+                <th>Max Occupancy</th>
+                <th></th>
+            </tr>
+        </thead>
+        
+        <tbody>
+            @if(count($rooms) > 0)
+                @foreach($rooms as $room)
+                    <tr>
+                        <th>{{$room->id}}</th>
+                        <td>{{$room->room_name}}</td>
+                        <td>{{$room->room_description}}</td>
+                        <td>{{$room->max_occupancy}}</td>
+                    </tr>
+                @endforeach
+            @else
+            <tr>
+                <th>No Rooms Found</th>
+            </tr>
+            @endif
+        </tbody>
+    </table>
 @endsection
