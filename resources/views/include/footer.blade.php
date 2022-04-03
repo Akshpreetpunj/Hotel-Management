3f3472bb410250351c4ac529ffe91fef48784cc2
Akshpreet Punj <000820040@mohawkcollege.ca>
2022-04-03 18:33:15 -0400

    Fixed Small Issues

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -1,3 +1,3 @@
-<footer class="bg-dark text-white footer mt-auto fixed-bottom">
+<footer class="bg-dark text-white footer mt-auto py-2 fixed-bottom">
     <p class="text-center">Copyright 2022</p>
 </footer>
