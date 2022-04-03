e64740a478abe735c18087d114f574302dc1b93e
Akshpreet Punj <000820040@mohawkcollege.ca>
2022-04-03 13:11:47 -0400

    Updated Web file

--- resources/views/rooms/index.blade.php
+++ resources/views/rooms/index.blade.php
@@ -23,23 +23,21 @@
                         <td>{{$room->room_name}}</td>
                         <td>{{$room->room_description}}</td>
                         <td>{{$room->max_occupancy}}</td>
-                        <td><a href="/rooms/{{$room->id}}/edit" type="button" class="btn btn-primary btn-sm">Edit</a>
+                        <!--<td><a href="/rooms/{{$room->id}}/edit" type="button" class="btn btn-primary btn-sm">Edit</a>-->
                             
                         </td>
-                        <!--
                         <td>
-                            <form action="{{ url('/rooms') }}" method="post">
+                            <form action="{{ url('/rooms')/ . $room->id }}" method="post">
                                 @csrf
                                 @method('DELETE')
                                 <button class="btn btn-danger" type="submit">Delete</button>
                             </form>
-                            {!! Form::open(['url' => '//still-journey-97048.herokuapp.com/rooms/' . $room->id, 'method'=> 'POST']) !!}
+                            <!--{!! Form::open(['url' => '//still-journey-97048.herokuapp.com/rooms/' . $room->id, 'method'=> 'POST']) !!}
                             @csrf
                             {{Form::hidden('_method', 'DELETE')}}
                                 {{Form::submit('Delete'), ['class' => 'btn btn-danger btn-sm']}}
-                            {!!Form::close()!!} 
+                            {!!Form::close()!!} -->
                         </td>
-                    -->
                     </tr>
                 @endforeach
             @else
