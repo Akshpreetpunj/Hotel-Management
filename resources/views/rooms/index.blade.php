2a2c63c0e15249fe2a3ccb17dae78c6eb5afb695
Akshpreet Punj <000820040@mohawkcollege.ca>
2022-04-03 13:17:16 -0400

    Updated Web file

--- resources/views/rooms/index.blade.php
+++ resources/views/rooms/index.blade.php
@@ -11,6 +11,7 @@
                 <th>Room Name</th>
                 <th>Room Description</th>
                 <th>Max Occupancy</th>
+                <th></th>
             </tr>
         </thead>
         
@@ -23,18 +24,14 @@
                         <td>{{$room->room_description}}</td>
                         <td>{{$room->max_occupancy}}</td>
                         <!--
-                        <td>
-                            <form action="{{ url('/rooms')}}" method="post">
-                                @csrf
-                                @method('DELETE')
-                                <button class="btn btn-danger" type="submit">Delete</button>
-                            </form>
+                        <td><a href="/rooms/{{$room->id}}/edit" type="button" class="btn btn-primary btn-sm">Edit</a>
                             {!! Form::open(['url' => '//still-journey-97048.herokuapp.com/rooms/' . $room->id, 'method'=> 'POST']) !!}
                             @csrf
                             {{Form::hidden('_method', 'DELETE')}}
                                 {{Form::submit('Delete'), ['class' => 'btn btn-danger btn-sm']}}
                             {!!Form::close()!!}
-                        </td> -->
+                        </td>
+-->
                     </tr>
                 @endforeach
             @else
