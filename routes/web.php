6cdbe2f34b243ec5664199f07c91bac8aecb833a
Akshpreet Punj <000820040@mohawkcollege.ca>
2022-04-02 21:43:58 -0400

    Updated web file

--- routes/web.php
+++ routes/web.php
@@ -27,5 +27,6 @@ Route:: get('/about', [AboutController::class, 'index'])->name('about');
 Route:: get('/rooms', [RoomsController::class, 'index'])->name('rooms');
 Route:: get('/bookings', [BookingsController::class, 'index'])->name('bookings');
 
-Route::resource('rooms', 'RoomsController');
+Route:: get('/rooms/create', [RoomsController::class, 'create']);
+//Route::resource('rooms', 'RoomsController');
 //Route::resource('bookings', 'BookingsController');
