f57abb351eac7b43be50a767240d22e7851001d3
Akshpreet Punj <000820040@mohawkcollege.ca>
2022-04-02 22:18:43 -0400

    Updated web Files

--- resources/views/rooms/create.blade.php
+++ resources/views/rooms/create.blade.php
@@ -6,19 +6,19 @@
     <form method="post" action="{{ url('/rooms') }}">
     @csrf
         <div class="form-group">
-            <label for="room_number">Room Number:</label>
-            <input type="text" class="form-control" name="room_number" id="room_number"/>
+            <label for="id">Room Number:</label>
+            <input type="text" class="form-control" name="id" id="id"/>
         </div>
         <div class="form-group">
             <label for="room_name">Room Name:</label>
             <input type="text" class="form-control" name="room_name" id="room_name"/>
         </div>
         <div class="form-group">
-            <label for="room_name">Room Description:</label>
+            <label for="room_description">Room Description:</label>
             <input type="text" class="form-control" name="room_description" id="room_description"/>
         </div>
         <div class="form-group">
-            <label for="room_name">Max Occupancy:</label>
+            <label for="max_occupancy">Max Occupancy:</label>
             <input type="text" class="form-control" name="max_occupancy" id="max_occupancy"/>
         </div>
         
