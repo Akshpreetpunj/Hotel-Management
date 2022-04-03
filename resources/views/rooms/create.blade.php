1b4ef7afd692c5e302e2684e3f1aed43fd0f002d
Akshpreet Punj <000820040@mohawkcollege.ca>
2022-04-02 21:56:40 -0400

    Updated web file

--- resources/views/rooms/create.blade.php
+++ resources/views/rooms/create.blade.php
@@ -3,25 +3,17 @@
 @section('content')
     <h1>Create Hotel Rooms</h1>
 
-    {!! Form::open(['url' => '//http://still-journey-97048.herokuapp.com/rooms', 'method'=> 'POST']) !!}
-  @csrf
-    <div class="form-group">
-      {{Form::label('id', 'Room Number')}}
-      {{Form::number('id', '', ['class' => 'form-control', 'placeholder' => 'Room Number'])}}
-    </div>
-    <div class="form-group">
-      {{Form::label('name', 'Room Name')}}
-      {{Form::text('name', '', ['class' => 'form-control', 'placeholder' => 'Room Name'])}}
-    </div>
-    <div class="form-group">
-      {{Form::label('description', 'Room Description')}}
-      {{Form::text('description', '', ['class' => 'form-control', 'placeholder' => 'Room Description'])}}
-    </div>
-    <div class="form-group">
-      {{Form::label('max_occupancy', 'Max Occupancy')}}
-      {{Form::number('max_occupancy', '', ['class' => 'form-control', 'placeholder' => 'Max Occupancy'])}}
-    </div>
-    {{Form::submit('Submit', ['class' => 'btm btn-primary'])}}
-  {!! Form::close() !!}
+    <form method="post" action="{{ url('/rooms') }}">
+          <div class="form-group">
+              @csrf
+              <label for="country_name">Game Name:</label>
+              <input type="text" class="form-control" name="name"/>
+          </div>
+          <div class="form-group">
+              <label for="cases">Price :</label>
+              <input type="text" class="form-control" name="price"/>
+          </div>
+          <button type="submit" class="btn btn-primary">Add Game</button>
+      </form>
 
 @endsection
