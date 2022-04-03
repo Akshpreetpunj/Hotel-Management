067b52909197d95f0651f388c443e4e6fd531e1d
Akshpreet Punj <000820040@mohawkcollege.ca>
2022-04-02 22:11:40 -0400

    Updated Create Room File

--- resources/views/rooms/create.blade.php
+++ resources/views/rooms/create.blade.php
@@ -4,16 +4,25 @@
     <h1>Create Hotel Rooms</h1>
 
     <form method="post" action="{{ url('/rooms') }}">
-          <div class="form-group">
-              @csrf
-              <label for="country_name">Game Name:</label>
-              <input type="text" class="form-control" name="name"/>
-          </div>
-          <div class="form-group">
-              <label for="cases">Price :</label>
-              <input type="text" class="form-control" name="price"/>
-          </div>
-          <button type="submit" class="btn btn-primary">Add Game</button>
+    @csrf
+        <div class="form-group">
+            <label for="room_number">Room Number:</label>
+            <input type="text" class="form-control" name="room_number" id="room_number"/>
+        </div>
+        <div class="form-group">
+            <label for="room_name">Room Name:</label>
+            <input type="text" class="form-control" name="room_name" id="room_name"/>
+        </div>
+        <div class="form-group">
+            <label for="room_name">Room Description:</label>
+            <input type="text" class="form-control" name="room_description" id="room_description"/>
+        </div>
+        <div class="form-group">
+            <label for="room_name">Max Occupancy:</label>
+            <input type="text" class="form-control" name="max_occupancy" id="max_occupancy"/>
+        </div>
+        
+        <button type="submit" class="btn btn-primary">Submit</button>
       </form>
 
 @endsection
