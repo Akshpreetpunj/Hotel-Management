ac1da1f75b59532a52860c285a7970f9e7c9b56b
Akshpreet Punj <000820040@mohawkcollege.ca>
2022-04-03 15:48:43 -0400

    Updated Rooms file

--- resources/views/rooms/edit.blade.php
+++ resources/views/rooms/edit.blade.php
@@ -2,4 +2,15 @@
 
 @section('content')
     <h1>Edit Hotel Room Description</h1>
+
+    <form method="post" action="{{ url('/rooms') }}">
+    @csrf
+    @method('PATCH')
+        <div class="form-group">
+            <label for="room_description">Room Description:</label>
+            <input type="text" class="form-control" name="room_description" id="room_description" placeholder="Room Description" value="{{ $room->room_description }}" required/>
+        </div>
+        
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
 @endsection
