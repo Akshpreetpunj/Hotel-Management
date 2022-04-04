ce79e6abb51ed3eb154eb2ef95b3269216ac00ba
Akshpreet Punj <000820040@mohawkcollege.ca>
2022-04-04 17:36:08 -0400

    Changed the heading color

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -1,7 +1,7 @@
 {{-- "StAuth10127: I Akshpreet Singh Punj, 000820040 certify that this material is my original work. No other person's work has been used without due acknowledgement. I have not made my work available to anyone else." --}}
 
 <div class="container">
-    <h1 class="text-center">
+    <h1 class="text-center text-dark">
         <a href="/" style="text-decoration:none">Lab 5 - Laravel Basics</a>
     </h1>
 </div>
